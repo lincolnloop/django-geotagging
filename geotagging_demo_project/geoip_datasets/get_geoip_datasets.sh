289df5c18fac9b12f0669c670db7e94debdee30c
Peter Baumgartner <pete@lincolnloop.com>
2010-03-18 13:04:43 -0600

    Removed legacy files to be replaced/rewritten at a later date

--- geotagging_demo_project/geoip_datasets/get_geoip_datasets.sh
+++
@@ -1,7 +0,0 @@
- #!/bin/sh
-
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
-wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCountry/GeoIP.dat.gz
-
-gunzip -d GeoIP.dat.gz
-gunzip -d GeoLiteCity.dat.gz
--- geotagging_demo_project/geotagging_tests/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class DummyModel(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return self.name
